f568bde1b242a1f9471690febff7aa58baa97c14
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-03-19 17:42:45 -0700

    repurposing just-tech-new api routes and handlebars for now, will start editing to fix

---
+++ models/index.js
@@ -0,0 +1,18 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+// create associations
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'CASCADE'
+});
+module.exports = {Post, User, Comment}
